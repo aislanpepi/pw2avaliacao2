f4d3bcc077be26ce79c6d5577ec2a0c296e7f73d
Aislan Pepi <aislan.pepi@outlook.com>
2024-10-22 09:47:40 -0300

    Updating CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,30 @@
 import React from "react";
-import './Header.css';
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'; // Optional, if you have any remaining custom styles
 
 const Header = () => {
     return (
-        <header >
-            <h1>Catalogo de Filmes</h1>
-            <nav className="header">
-                <ul>
-                    <li><Link to="/">Início</Link></li>
-                    <li><Link to="./create">Criar</Link></li>
-                    <li><Link to="/update">Atualizar</Link></li>
-                    <li><Link to="/delete">Deletar</Link></li>
-                </ul>
-            </nav>
+        <header className="bg-dark text-white py-3 rounded mb-4 border border-dark">
+            <div className="container">
+                <h1 className="text-center mb-4">Catálogo de Filmes</h1>
+                <nav>
+                    <ul className="nav justify-content-center">
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" to="/">Início</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" to="/create">Criar</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" to="/update">Atualizar</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" to="/delete">Deletar</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
